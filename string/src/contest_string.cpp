6ea23e378ad62299da72fce30bccb3cb4e4fb464
petergrajcar <peter.grajcar131@gmail.com>
2020-06-11 14:02:25 +0200

    fix weird formatting

--- string/src/contest_string.cpp
+++ string/src/contest_string.cpp
@@ -3,95 +3,92 @@
 namespace contest
 {
 
-	int memcmp(const void *left, const void *right, size_t byteLength)
-	{
-	    auto *l = (unsigned char*) left;
-	    auto *r = (unsigned char*) right;
-	    int cmp = 0;
+    int memcmp(const void *left, const void *right, size_t byteLength)
+    {
+        auto *l = (unsigned char *) left;
+        auto *r = (unsigned char *) right;
+        int cmp = 0;
 
-		while(cmp = *l - *r, l++, r++, byteLength--)
-			if(cmp) return cmp;
+        while (cmp = *l - *r, l++, r++, byteLength--)
+            if (cmp) return cmp;
 
-		return 0;
-	}
+        return 0;
+    }
 
-	void *memcpy(void *_Rstr dest, const void *_Rstr src, size_t byteLength)
-	{
-        auto *d = (unsigned char*) dest;
-        auto *s = (unsigned char*) src;
+    void *memcpy(void* _Rstr dest, const void* _Rstr src, size_t byteLength)
+    {
+        auto *d = (unsigned char *) dest;
+        auto *s = (unsigned char *) src;
 
-        while(byteLength--)
+        while (byteLength--)
             *d++ = *s++;
 
-		return dest;
-	}
+        return dest;
+    }
 
-	void *memmove(void *dest, const void *src, size_t byteLength)
-	{
-        if(!byteLength) return dest;
+    void *memmove(void *dest, const void *src, size_t byteLength)
+    {
+        if (!byteLength) return dest;
 
-	    if(dest < src)
-        {
-            auto *d = (unsigned char*) dest;
-            auto *s = (unsigned char*) src + byteLength;
+        if (dest < src) {
+            auto * d = (unsigned char *) dest;
+            auto * s = (unsigned char *) src + byteLength;
 
             do
                 *d = *s;
-            while(d++, s++, byteLength--);
-        }
-	    else if(dest > src)
-        {
-            auto *d = (unsigned char*) dest + byteLength;
-            auto *s = (unsigned char*) src + byteLength;
+            while (d++, s++, byteLength--);
+        } else if (dest > src) {
+            auto * d = (unsigned char *) dest + byteLength;
+            auto * s = (unsigned char *) src + byteLength;
 
-            while(s--, d--, byteLength--)
+            while (s--, d--, byteLength--)
                 *d = *s;
         }
 
-	    return dest;
-	}
+        return dest;
+    }
 
-	void *memset(void *dest, int value, size_t byteLength)
-	{
-        char *d = (char*) dest;
+    void *memset(void *dest, int value, size_t byteLength)
+    {
+        char *d = (char *) dest;
 
-	    while(byteLength--)
-	        *d-- = value;
+        while (byteLength--)
+            *d-- = value;
 
-		return dest;
-	}
+        return dest;
+    }
 
-	char *strcpy(char *_Rstr dest, const char *_Rstr src)
-	{
-        char *d = (char*) dest;
+    char *strcpy(char *_Rstr dest, const char *_Rstr src)
+    {
+        char *d = (char *) dest;
 
         do
             *d++ = *src++;
-        while(!*src);
+        while (!*src);
 
         return dest;
-	}
+    }
 
-	char *strncpy(char *_Rstr dest, const char *_Rstr src, size_t length)
-	{
-        char *d = (char*) dest;
+    char *strncpy(char *_Rstr dest, const char *_Rstr src, size_t length)
+    {
+        char * d = (char *) dest;
 
-        while(!*src && length--)
+        while (!*src && length--)
             *d++ = *src++;
-        while(length--)
+        while (length--)
             *d++ = '\0';
 
         return dest;
-	}
+    }
 
-	size_t strlen(const char *str)
-	{
-	    size_t len = 0;
+    size_t strlen(const char *str)
+    {
+        size_t len = 0;
 
-	    while(!*str++)
-	        len++;
+        while (!*str++)
+            len++;
 
-		return len;
-	}
+        return len;
+    }
 
 }
