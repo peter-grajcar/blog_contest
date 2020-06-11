5ffa251ebdfb4b4479349b763acd44a73dafeb98
petergrajcar <peter.grajcar131@gmail.com>
2020-06-11 13:58:11 +0200

    implement contest_string

--- string/src/contest_string.cpp
+++ string/src/contest_string.cpp
@@ -5,37 +5,93 @@ namespace contest
 
 	int memcmp(const void *left, const void *right, size_t byteLength)
 	{
+	    auto *l = (unsigned char*) left;
+	    auto *r = (unsigned char*) right;
+	    int cmp = 0;
+
+		while(cmp = *l - *r, l++, r++, byteLength--)
+			if(cmp) return cmp;
+
 		return 0;
 	}
 
 	void *memcpy(void *_Rstr dest, const void *_Rstr src, size_t byteLength)
 	{
-		return nullptr;
+        auto *d = (unsigned char*) dest;
+        auto *s = (unsigned char*) src;
+
+        while(byteLength--)
+            *d++ = *s++;
+
+		return dest;
 	}
 
 	void *memmove(void *dest, const void *src, size_t byteLength)
 	{
-		return nullptr;
+        if(!byteLength) return dest;
+
+	    if(dest < src)
+        {
+            auto *d = (unsigned char*) dest;
+            auto *s = (unsigned char*) src + byteLength;
+
+            do
+                *d = *s;
+            while(d++, s++, byteLength--);
+        }
+	    else if(dest > src)
+        {
+            auto *d = (unsigned char*) dest + byteLength;
+            auto *s = (unsigned char*) src + byteLength;
+
+            while(s--, d--, byteLength--)
+                *d = *s;
+        }
+
+	    return dest;
 	}
 
 	void *memset(void *dest, int value, size_t byteLength)
 	{
-		return nullptr;
+        char *d = (char*) dest;
+
+	    while(byteLength--)
+	        *d-- = value;
+
+		return dest;
 	}
 
 	char *strcpy(char *_Rstr dest, const char *_Rstr src)
 	{
-		return nullptr;
+        char *d = (char*) dest;
+
+        do
+            *d++ = *src++;
+        while(!*src);
+
+        return dest;
 	}
 
 	char *strncpy(char *_Rstr dest, const char *_Rstr src, size_t length)
 	{
-		return nullptr;
+        char *d = (char*) dest;
+
+        while(!*src && length--)
+            *d++ = *src++;
+        while(length--)
+            *d++ = '\0';
+
+        return dest;
 	}
 
 	size_t strlen(const char *str)
 	{
-		return 0;
+	    size_t len = 0;
+
+	    while(!*str++)
+	        len++;
+
+		return len;
 	}
 
 }
--- string_test.cpp
+++ string_test.cpp
@@ -25,6 +25,17 @@ int main()
 			<< "And good luck, with lots of love <3\n"
 			<< "Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>\n";
 
+	int mem1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int mem2[5];
+    int mem3[] = {1, 2, 3, 4, 5, 8, 9, 10, 11, 12};
+    int mem4[20];
+    char str1[] = "Milan";
+    char str2[] = "Hello, _____!";
+    char str3[6] = "";
+    char str4[21] = "____________________";
+    char str5[] = "";
+
+    /*
 	contest::memcmp(nullptr, nullptr, 0);
 	contest::memcpy(nullptr, nullptr, 0);
 	contest::memmove(nullptr, nullptr, 0);
@@ -32,6 +43,50 @@ int main()
 	contest::strcpy(nullptr, nullptr);
 	contest::strncpy(nullptr, nullptr, 0);
 	contest::strlen(nullptr);
+     */
+
+    memcpy(mem2, mem1, 5*sizeof(int));
+    memmove(mem1 + 5, mem1, 5*sizeof(int));
+    memmove(mem3, mem3 + 5, 5*sizeof(int));
+    memset(mem4, 0, 20 * sizeof(int));
+    strcpy(str3, str1);
+    strncpy(str2 + 7, str1, 5);
+    strncpy(str4, str1, 20);
+
+	std::cout << "mem1: ";
+	for(int i : mem1)
+	    std::cout << i << ' ';
+	std::cout << std::endl;
+
+    std::cout << "mem2: ";
+    for(int i : mem2)
+        std::cout << i << ' ';
+    std::cout << std::endl;
+
+    std::cout << "mem3: ";
+    for(int i : mem3)
+        std::cout << i << ' ';
+    std::cout << std::endl;
+
+    std::cout << "mem4: ";
+    for(int i : mem4)
+        std::cout << i << ' ';
+    std::cout << std::endl;
+
+    std::cout << "str1: " << str1 << std::endl
+              << "\tlength: " << strlen(str1) << std::endl
+              << "str2: " << str2 << std::endl
+              << "\tlength: " << strlen(str2) << std::endl
+              << "str3: " << str3 << std::endl
+              << "\tlength: " << strlen(str3) << std::endl
+              << "str4: ";
+
+    for(char c : str4)
+        std::cout << c;
+    std::cout << std::endl;
+    std::cout << "\tlength: " << strlen(str4) << std::endl;
+
+    std::cout << "str5 length: " << strlen(str5) << std::endl;
 
 	return 0;
 }
